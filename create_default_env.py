72a89645343807e36a3f3a19fabdf8f8b71d0d31
jameshlms <103217525+jameshlms@users.noreply.github.com>
2025-09-14 19:59:42 -0400

    Fixed create_default_env

--- create_default_env.py
+++ create_default_env.py
@@ -7,13 +7,13 @@ def create_default_env(overwrite: bool = False) -> bool:
         return False
     with open(".env", "w") as env:
         env.writelines(
-            [
-                "RANDOM_STATE=42\n",
-                "TRAINING_FRACTION=0.8\n",
+            (
+                "RANDOM_STATE=39103\n",
+                "TRAINING_FRACTION=0.75\n",
                 "RAW_DATA_PATH=../data/raw/creditcard.csv\n",
                 "TRAIN_DATA_PATH=../data/processed/train.parquet\n",
                 "TEST_DATA_PATH=../data/processed/test.parquet\n",
-            ]
+            )
         )
 
     return True
