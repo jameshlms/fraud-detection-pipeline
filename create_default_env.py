777cfa9cee1e7945f97e287c27370dbc70f943e4
jameshlms <103217525+jameshlms@users.noreply.github.com>
2025-09-14 19:56:57 -0400

    Created default env initialization

---
+++ create_default_env.py
@@ -0,0 +1,23 @@
+import os
+
+
+def create_default_env(overwrite: bool = False) -> bool:
+    if not overwrite and os.path.exists(".env"):
+        print(".env file already exists. Use overwrite=True to overwrite.")
+        return False
+    with open(".env", "w") as env:
+        env.writelines(
+            [
+                "RANDOM_STATE=42\n",
+                "TRAINING_FRACTION=0.8\n",
+                "RAW_DATA_PATH=../data/raw/creditcard.csv\n",
+                "TRAIN_DATA_PATH=../data/processed/train.parquet\n",
+                "TEST_DATA_PATH=../data/processed/test.parquet\n",
+            ]
+        )
+
+    return True
+
+
+if __name__ == "__main__":
+    result = create_default_env()
