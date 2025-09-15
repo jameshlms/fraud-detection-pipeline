4d1115cbcf3835e9a34882b957bb6f5f1a5c37ed
jameshlms <103217525+jameshlms@users.noreply.github.com>
2025-09-14 21:58:10 -0400

    Updated paths in split_data

--- training/split_data.py
+++ training/split_data.py
@@ -6,10 +6,14 @@ from sklearn.model_selection import train_test_split
 
 load_dotenv()
 RANDOM_STATE = int(getenv("RANDOM_STATE", 0))
+TRAINING_FRACTION = float(getenv("TRAINING_FRACTION", 0.75))
+RAW_DATA_PATH = getenv("RAW_DATA_PATH", "./data/raw/creditcard.csv")
+TRAIN_DATA_PATH = getenv("TRAIN_DATA_PATH", "./data/processed/train.parquet")
+TEST_DATA_PATH = getenv("TEST_DATA_PATH", "./data/processed/test.parquet")
 
 
 def main(*args, **kwargs) -> None:
-    df = read_csv(r"../data/raw/creditcard.csv")
+    df = read_csv(RAW_DATA_PATH)
 
     X: DataFrame = df.drop(columns=["Class"])
     y: Series = df["Class"]
@@ -17,7 +21,7 @@ def main(*args, **kwargs) -> None:
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
-        test_size=kwargs.get("test_size", 0.2),
+        train_size=kwargs.get("train_size", TRAINING_FRACTION),
         random_state=kwargs.get("random_state", RANDOM_STATE),
         stratify=y,
     )
@@ -27,8 +31,8 @@ def main(*args, **kwargs) -> None:
     test_df = DataFrame(X_test)
     test_df["Class"] = y_test.values
 
-    train_df.to_parquet(r"../data/processed/train.parquet", index=False)
-    test_df.to_parquet(r"../data/processed/test.parquet", index=False)
+    train_df.to_parquet(TRAIN_DATA_PATH, index=False)
+    test_df.to_parquet(TEST_DATA_PATH, index=False)
 
 
 if __name__ == "__main__":
